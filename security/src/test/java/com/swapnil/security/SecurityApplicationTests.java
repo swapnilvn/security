0080c7cdec96ba01ab98e311ede3c88566224a35
Swapnil Nagothanekar <itsaswapnil@gmail.com>
2025-10-15 11:23:14 +0530

    init project

---
+++ security/src/test/java/com/swapnil/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapnil.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
