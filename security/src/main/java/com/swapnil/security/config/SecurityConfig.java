ffda66dcf7c1269a765effc96de41c4b384d36a4
Swapnil Nagothanekar <itsaswapnil@gmail.com>
2025-10-16 00:37:11 +0530

    basic authentication credentials in properties file

--- security/src/main/java/com/swapnil/security/config/SecurityConfig.java
+++ security/src/main/java/com/swapnil/security/config/SecurityConfig.java
@@ -2,17 +2,22 @@ package com.swapnil.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-            .formLogin(form -> form.loginPage("/public").permitAll())
-            .logout(logout -> logout.permitAll());
-        return null;
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> (requests.anyRequest()).authenticated());
+        http.httpBasic(Customizer.withDefaults());
+        http.sessionManagement(session
+                -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
     }
 }
