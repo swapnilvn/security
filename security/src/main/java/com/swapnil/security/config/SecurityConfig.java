aff617611b9a10b1bf551b3da5c34b4eb0d39204
Swapnil Nagothanekar <itsaswapnil@gmail.com>
2025-10-23 23:37:13 +0530

    hardcoded credentials in java class

--- security/src/main/java/com/swapnil/security/config/SecurityConfig.java
+++ security/src/main/java/com/swapnil/security/config/SecurityConfig.java
@@ -2,11 +2,23 @@ package com.swapnil.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -23,5 +35,16 @@ public class SecurityConfig {
         http.headers(header -> header.frameOptions(frame -> frame.sameOrigin()));
         http.csrf(csrf -> csrf.disable());
         return http.build();
+    }
+
+    @Bean
+    UserDetailsService getUserDetailsService(){
+        UserDetails build = User.builder().username("swapnil").password(passwordEncoder().encode("aaa")).roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(build);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
